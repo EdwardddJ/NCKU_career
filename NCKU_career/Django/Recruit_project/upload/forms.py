746a6b59092fde11416ff32344ac08ef788bf0df
Edward <theedward285@gmail.com>
2017-12-21 14:28:40 +0800

    renove NCKU_career Django

--- NCKU_career/Django/Recruit_project/upload/forms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django import forms
-
-class DocumentForm(forms.Form):
-    docfile = forms.FileField(
-        label='',
-        help_text='請依照 "公司名稱_檔名" 命名 , 已加速審核謝謝! (大小上限 : 42 megabytes)'
-    )
